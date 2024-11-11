7b3a34c12ae5fb44eaf2df7df0568a94e1a04e32
Smoot-Mena <Msmoot810@gmail.com>
2024-11-11 11:45:29 -0500

    Partially Sets Up Navbar Design
    Starts the look of the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,77 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import Logout from './Logout';
 
+import Nav from "react-bootstrap/Nav";
+import Stack from 'react-bootstrap/Stack';
+
 const Navbar = () => {
   return (
-    <nav>
-        <Link to={"/"}>Home</Link>
-        <ul>
-            <li><Link to={"/tickets"}>Tickets</Link></li>
-            <li><Link to={"/users"}>Users</Link></li>
-            <li><Link to={"/admins"}>Admins</Link></li>
-            <li><Link to={"/faq"}>FAQ</Link></li>
-        </ul>
-        <Logout />
-    </nav>
+    <>
+      <Nav variant='pills' fill defaultActiveKey={"/home"}>
+        <Nav.Item>
+          <Nav.Link href='/'>Home</Nav.Link>
+        </Nav.Item>
+
+        <Nav.Item>
+          <Nav.Link href='/tickets'>Tickets</Nav.Link>
+        </Nav.Item>
+
+        <Nav.Item>
+          <Nav.Link href='/users'>Users</Nav.Link>
+        </Nav.Item>
+
+        <Nav.Item>
+          <Nav.Link href='/admins'>Admins</Nav.Link>
+        </Nav.Item>
+
+        <Nav.Item>
+          <Nav.Link href='/faq'>FAQ</Nav.Link>
+        </Nav.Item>
+
+        <Nav.Item>
+          <Nav.Link>
+            <Logout />
+          </Nav.Link>
+        </Nav.Item>
+
+      </Nav>
+    </>
   )
 }
 
 export default Navbar;
+
+{/* <nav>
+<Stack direction='horizontal' gap={5}>
+<Link to={"/"}>Home</Link>
+  <Stack direction='horizontal' gap={5}>
+    <Link to={"/tickets"}>Tickets</Link>
+    <Link to={"/users"}>Users</Link>
+    <Link to={"/admins"}>Admins</Link>
+    <Link to={"/faq"}>FAQ</Link>
+  </Stack>
+  <Logout />
+</Stack>
+</nav> 
+
+        <Nav.Item>
+          <Nav as={"ul"}>
+            <Nav.Item as={"li"}>
+              <Nav.Link href='/tickets'>Tickets</Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item as={"li"}>
+              <Nav.Link href='/users'>Users</Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item as={"li"}>
+              <Nav.Link href='/admins'>Admins</Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item as={"li"}>
+              <Nav.Link href='/faq'>FAQ</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Nav.Item>
+
+*/}
